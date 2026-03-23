2650874ddc667d8b2e9f530218b035c2b18287fa
DangKhoa2410 <dangkhoa241024@gmail.com>
2026-03-24 00:21:09 +0700

    refactor:update metada for upComingPage

---
+++ cinevora-web/src/app/phim-sap-chieu/UpComingClient.tsx
@@ -0,0 +1,48 @@
+'use client'
+
+import MovieCard from "@/src/components/common/movieCard";
+import ShowtimeScheduleModal from "@/src/components/ShowtimeScheduleModal";
+import { useListUpComingMovies } from "@/src/hooks/Movies/useListUpComingMovies";
+import { useState } from "react";
+
+const UpComingClient = () => {
+  const [openShowtime, setOpenShowtime] = useState(false);
+  const { movies = [], isErrorMovie, isLoadingMovie } = useListUpComingMovies();
+
+  if (isErrorMovie) return <div>Lỗi tải dữ liệu</div>;
+
+  const handleBooking = () => {
+    setOpenShowtime(true);
+  };
+
+  return (
+    <div className="ml-6">
+      <div className="pt-[40px] mb-[25px] md:pt-[55px] lg:pt-[40px] lg:mb-[35px]">
+        <h2 className="tracking-tight uppercase font-bold font-saira text-[25px] md:text-[32px] lg:text-[24px]">
+          phim sắp chiếu
+        </h2>
+      </div>
+
+      {isLoadingMovie ? (
+        <p>Đang tải dữ liệu...</p>
+      ) : (
+        <div>
+          <MovieCard
+            movies={movies}
+            isShowOnlyBookBtn
+            onBooking={handleBooking}
+            isShowGenre
+            isShowBtn
+          />
+        </div>
+      )}
+
+      <ShowtimeScheduleModal
+        isModalOpen={openShowtime}
+        setIsModalOpen={setOpenShowtime}
+      />
+    </div>
+  );
+};
+
+export default UpComingClient;
