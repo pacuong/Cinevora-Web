0865c4e26ba2a6df7323a1ac3e782cba318c93a2
pacuong <cuongphan.240104@gmail.com>
2026-03-26 20:23:18 +0700

    feat: next config

--- cinevora-web/next.config.ts
+++ cinevora-web/next.config.ts
@@ -1,10 +1,13 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
-  //TODO: REMOVE DOMAIN
   images: {
-    domains: ['metiz.vn'],
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "**",
+      },
+    ],
   },
 };
 
